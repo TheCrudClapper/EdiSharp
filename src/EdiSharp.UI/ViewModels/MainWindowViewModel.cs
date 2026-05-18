815b2c9bb5f7d487d079d8d331e351898036910b
VortexOoN <wojciechmucha12@gmail.com>
2026-05-18 15:39:44 +0200

    build: update AvaloniaUI to version 12

--- src/EdiSharp.UI/ViewModels/MainWindowViewModel.cs
+++ src/EdiSharp.UI/ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,40 @@
-﻿namespace EdiSharp.UI.ViewModels;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Threading.Tasks;
+
+namespace EdiSharp.UI.ViewModels;
 
 public partial class MainWindowViewModel : ViewModelBase
 {
     
+    //MVVM Fields
+
+    [ObservableProperty]
+    private bool _validate = false;
+
+    [ObservableProperty]
+    private bool _showRawSegments = false;
+
+    [ObservableProperty]
+    private bool _isJsonChecked = false;
+
+    [ObservableProperty]
+    private bool _isXmlChecked = false;
+
+    [ObservableProperty]
+    private string _fileName = "Choose EDIFACT File";
+
+    [RelayCommand]
+    public async Task PickFile()
+    {
+        Console.WriteLine("Picking File");
+    }
+    
+    [RelayCommand]
+    public async Task Parse() 
+    {
+        Console.WriteLine("Hello");
+    }
+
 }
