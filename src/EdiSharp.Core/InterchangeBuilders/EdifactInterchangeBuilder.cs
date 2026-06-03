e5b1451ce3328515e5c6d6ba4d82263317d18be5
VortexOoN <wojciechmucha12@gmail.com>
2026-06-03 17:33:15 +0200

    feat(Core): create improved version of EdifactInterchangeBuilder

--- src/EdiSharp.Core/InterchangeBuilders/EdifactInterchangeBuilder.cs
+++ src/EdiSharp.Core/InterchangeBuilders/EdifactInterchangeBuilder.cs
@@ -1,5 +1,6 @@
 ﻿using EdiSharp.Core.Abstractions;
 using EdiSharp.Core.Enums;
+using EdiSharp.Core.Exceptions;
 using EdiSharp.Core.Models.IntermediateModel;
 using System.Globalization;
 
@@ -8,91 +9,121 @@ namespace EdiSharp.Core.MessageSplitters;
 public class EdifactInterchangeBuilder : IEdiInterchangeBuilder
 {
     public EdiStandard InputType => EdiStandard.EDIFACT;
+    private sealed class BuildContext
+    {
+        public InterchangeHeader? InterchangeHeader { get; set; }
+        public InterchangeTrailer? InterchangeTrailer { get; set; }
+        public List<EdiMessage> Messages { get; } = [];
+        public List<EdiSegment> CurrentMessageSegments { get; set; } = [];
+        public MessageHeader? MessageHeader { get; set; }
+        public MessageTrailer? MessageTrailer { get; set; }
+    }
 
     public EdiInterchange Build(List<EdiSegment> segments)
     {
-        EdiInterchange? interchange = null;
-        List<EdiMessage> messages = [];
-        List<EdiSegment> currentSegments = [];
-        string? messageRefNumber = null;
-        MessageIdentifier? messageIdentifier = null;
-        InterchangeHeader? interchangeHeader = null;
-        InterchangeTrailer? interchangeTrailer = null;
-        EdiMessage? currentMessage = null;
+        var ctx = new BuildContext();
 
-        foreach(var seg in segments)
+        foreach (var seg in segments)
         {
-            if (seg.Tag == "UNB")
+            switch (seg.Tag)
             {
-                var date = seg.Elements[3].Components[0];
-                var time = seg.Elements[3].Components[1];
-                interchangeHeader = new InterchangeHeader()
-                {
-                    SenderId = seg.Elements[1].Components[0],
-                    RecieverId = seg.Elements[2].Components[0],
-                    ControlReference = seg.Elements[4].Components[0],
-                    DateTime = DateTime.ParseExact(date + time, "yyMMddHHmm", CultureInfo.InvariantCulture)
-                };
-
-                continue;
+                case "UNB":
+                    HandleUNB(seg, ctx);
+                    break;
+                case "UNH":
+                    HandleUNH(seg, ctx);
+                    break;
+                case "UNT":
+                    HandleUNT(seg, ctx);
+                    break;
+                case "UNZ":
+                    HandleUNZ(seg, ctx);
+                    break;
+                default:
+                    HandlePayload(seg, ctx);
+                    break;
             }
-
-            if(seg.Tag == "UNH") 
-            {
-                messageRefNumber = seg.Elements[0].Components[0];
-                messageIdentifier = new MessageIdentifier()
-                {
-                    Type = seg.Elements[1].Components[0],
-                    Version = seg.Elements[1].Components[1],
-                    Release = seg.Elements[1].Components[2]
-                };
-
-                continue;
-            }
-
-            if(seg.Tag == "UNT")
-            {
-                currentMessage = new EdiMessage()
-                {
-                    Identifier = messageIdentifier,
-                    ReferenceNumber = messageRefNumber,
-                    Segments = currentSegments.ToList(),
-                };
-
-
-
-                messages.Add(currentMessage);
-
-                currentSegments.Clear();
-                currentMessage = null;
-                messageIdentifier = null;
-                messageRefNumber = null;
-
-                continue;
-            }
-
-            if(seg.Tag == "UNZ") 
-            {
-                interchangeTrailer = new InterchangeTrailer()
-                {
-                    MessageCount = int.Parse(seg.Elements[0].Components[0]),
-                    ControlReference = seg.Elements[1].Components[0],
-                };
-
-                interchange = new EdiInterchange()
-                {
-                    Header = interchangeHeader,
-                    Messages = messages,
-                    Trailer = interchangeTrailer,
-                    Standard = EdiStandard.EDIFACT,
-                };
-
-                continue;
-            }
-
-            currentSegments.Add(seg);
         }
 
-        return interchange;
+        return new EdiInterchange
+        {
+            Header = ctx.InterchangeHeader ?? throw new EdiSemanticsException("Missing UNB"),
+            Trailer = ctx.InterchangeTrailer ?? throw new EdiSemanticsException("Missing UNZ"),
+            Messages = ctx.Messages,
+            Standard = EdiStandard.EDIFACT
+        };
+    }
+
+    private static void HandleUNB(EdiSegment seg, BuildContext ctx)
+    {
+        var date = seg.Elements[3].Components[0];
+        var time = seg.Elements[3].Components[1];
+
+        ctx.InterchangeHeader = new InterchangeHeader
+        {
+            SenderId = seg.Elements[1].Components[0],
+            RecieverId = seg.Elements[2].Components[0],
+            ControlReference = seg.Elements[4].Components[0],
+            DateTime = DateTime.ParseExact(date + time, "yyMMddHHmm", CultureInfo.InvariantCulture)
+        };
+    }
+
+    private static void HandleUNH(EdiSegment seg, BuildContext ctx)
+    {
+        var identifier = seg.Elements[1];
+
+        ctx.MessageHeader = new MessageHeader()
+        {
+            MessageReferenceNumber = seg.Elements[0].Components[0],
+            Type = identifier.Components[0],
+            Version = identifier.Components[1],
+            Release = identifier.Components[2]
+        };
+
+        ctx.CurrentMessageSegments = [];
+    }
+
+    private static void HandleUNT(EdiSegment seg, BuildContext ctx)
+    {
+        if (ctx.MessageHeader is null)
+            throw new EdiSemanticsException("UNT without UNH");
+
+        ctx.MessageTrailer = new MessageTrailer()
+        {
+            //Add one, to count UNH as well
+            SegmentCount = int.Parse(seg.Elements[0].Components[0]) + 1,
+            MessageReferenceNumber = seg.Elements[1].Components[0]
+        };
+
+        var message = new EdiMessage()
+        {
+            Trailer = ctx.MessageTrailer,
+            Header = ctx.MessageHeader,
+            Segments = ctx.CurrentMessageSegments.ToList()
+        };
+
+        ctx.Messages.Add(message);
+
+        ctx.MessageTrailer = null;
+        ctx.MessageHeader = null;
+        ctx.CurrentMessageSegments = [];
+    }
+
+    private static void HandleUNZ(EdiSegment seg, BuildContext ctx)
+    {
+        ctx.InterchangeTrailer = new InterchangeTrailer()
+        {
+            MessageCount = int.Parse(seg.Elements[0].Components[0]),
+            ControlReference = seg.Elements[1].Components[0],
+        };
+    }
+
+    private static void HandlePayload(EdiSegment seg, BuildContext ctx) 
+    {
+        //Only when we are inside of message
+        if(ctx.MessageHeader is not null) 
+        {
+            ctx.CurrentMessageSegments.Add(seg);
+        }
     }
 }
--- src/EdiSharp.Core/Models/IntermediateModel/MessageHeader.cs
+++ src/EdiSharp.Core/Models/IntermediateModel/MessageHeader.cs
@@ -1,7 +1,9 @@
 ﻿namespace EdiSharp.Core.Models.IntermediateModel;
-public record MessageIdentifier
+
+public record MessageHeader
 {
+    public string? MessageReferenceNumber { get; init; }
     public required string Type { get; init; }
     public string? Version { get; init; }
-    public string? Release { get; init;  }
+    public string? Release { get; init; }
 }
